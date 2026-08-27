2c82fa2f3cd2bdef7d98f45c2272ef1158273f61
LuferOS <luisfernandoguzmannino@gmail.com>
2026-08-27 13:47:20 -0500

    build: bump version to 2.0 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.lumimusic.xqzpnm"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "1.5"
+    versionCode = 7
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
